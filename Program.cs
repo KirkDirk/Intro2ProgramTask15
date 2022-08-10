a9598d6ff1afd8641785b676531552d7c2c77c78
Kirk Dirk <jackalex@ya.ru>
2022-08-11 00:25:25 +0500

    add code

--- Program.cs
+++ Program.cs
@@ -3,3 +3,9 @@
 // 7 -> да
 // 1 -> нет
 
+Console.WriteLine("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num < 1) ^ (num > 7)) Console.WriteLine("Введён некорректный номер");
+else if ((num == 6) ^ (num==7)) Console.WriteLine("Ура! Выходной!");
+else Console.WriteLine("Работаем!");
